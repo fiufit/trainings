837ade13b1545167d0797c44f4140ed16745fb30
TGRodriguez <tgrodriguez@fi.uba.ar>
2023-05-03 00:35:52 -0300

    Added an error test for create training uc

--- usecases/create_training_test.go
+++ usecases/create_training_test.go
@@ -2,11 +2,13 @@ package usecases
 
 import (
 	"context"
+	"errors"
 	"testing"
 	"time"
 
 	"github.com/fiufit/trainings/contracts/training"
 	"github.com/fiufit/trainings/helpers"
+	"github.com/fiufit/trainings/models"
 	"github.com/fiufit/trainings/repositories/mocks"
 	"github.com/stretchr/testify/assert"
 	"github.com/undefinedlabs/go-mpatch"
@@ -36,4 +38,29 @@ func TestCreateTrainingOk(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Equal(t, res.TrainingPlan, training)
+}
+
+func TestCreateTrainingError(t *testing.T) {
+
+	ctx := context.Background()
+	creationDate := time.Now()
+	mpatch.PatchMethod(time.Now, func() time.Time {
+		return creationDate
+	})
+	req := training.CreateTrainingRequest{
+		Name:        "Test Name",
+		Description: "Test Description",
+		TrainerID:   "Test Trainer",
+		Exercises:   []training.ExerciseRequest{},
+	}
+	trainingRepo := new(mocks.TrainingPlans)
+
+	training := helpers.ConverToTrainingPlan(req, helpers.ConvertToExercises(req.Exercises))
+	trainingRepo.On("CreateTrainingPlan", ctx, training).Return(models.TrainingPlan{}, errors.New("repo error"))
+
+	trainingUc := NewTrainingCreatorImpl(trainingRepo, zaptest.NewLogger(t))
+	res, err := trainingUc.CreateTraining(ctx, req)
+
+	assert.Equal(t, res.TrainingPlan, models.TrainingPlan{})
+	assert.Error(t, err)
 }

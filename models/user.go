48384810bb99c89548c494d3b7e3797906045300
TGRodriguez <tgrodriguez@fi.uba.ar>
2023-05-18 15:54:02 -0300

    Added pictureUrl field and removed gorm tags

--- models/user.go
+++ models/user.go
@@ -7,16 +7,17 @@ import (
 )
 
 type User struct {
-	ID                string    `gorm:"primaryKey;not null"`
-	Nickname          string    `gorm:"not null;unique;index"`
-	DisplayName       string    `gorm:"not null"`
-	IsMale            bool      `gorm:"not null"`
-	CreatedAt         time.Time `gorm:"not null"`
+	ID                string
+	Nickname          string
+	DisplayName       string
+	IsMale            bool
+	CreatedAt         time.Time
 	DeletedAt         gorm.DeletedAt
-	BornAt            time.Time `gorm:"not null"`
-	Height            uint      `gorm:"not null"`
-	Weight            uint      `gorm:"not null"`
-	IsVerifiedTrainer bool      `gorm:"not null;default:false"`
-	MainLocation      string    `gorm:"not null"`
-	Interests         []string  `gorm:"-"`
+	BornAt            time.Time
+	Height            uint
+	Weight            uint
+	IsVerifiedTrainer bool
+	MainLocation      string
+	Interests         []string
+	PictureUrl        string
 }

9849a14309b8a1e07e0368cab44e8174ffa19003
TGRodriguez <tgrodriguez@fi.uba.ar>
2023-05-30 21:28:06 -0300

    Removed uniqueIndex tag for training plan version

--- models/review.go
+++ models/review.go
@@ -6,7 +6,7 @@ type Review struct {
 	ID                  uint      `gorm:"primaryKey"`
 	CreatedAt           time.Time `gorm:"not null"`
 	TrainingPlanID      uint      `gorm:"not null;uniqueIndex:idx_user_id_training_plan_id_training_plan_version"`
-	TrainingPlanVersion uint      `gorm:"not null;uniqueIndex:idx_user_id_training_plan_id_training_plan_version"`
+	TrainingPlanVersion uint      `gorm:"not null"`
 	UserID              string    `gorm:"not null;uniqueIndex:idx_user_id_training_plan_id_training_plan_version" json:"-"`
 	User                User      `gorm:"-"`
 	Score               uint      `gorm:"not null"`

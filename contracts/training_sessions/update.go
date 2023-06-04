eb0cf46fc16ea9a01da0e2b352293c110417f52f
guillermo-st <guillermo_stancanelli@outlook.com>
2023-06-04 17:54:10 -0300

    fix zero value binding for session requests

--- contracts/training_sessions/update.go
+++ contracts/training_sessions/update.go
@@ -10,8 +10,8 @@ type UpdateTrainingSessionRequest struct {
 	RequesterID      string                         `json:"requester_id" binding:"required"`
 	ExerciseSessions []UpdateExerciseSessionRequest `json:"exercise_sessions" binding:"required"`
 	Done             *bool                          `json:"done" binding:"required"`
-	StepCount        uint                           `json:"step_count" binding:"required"`
-	SecondsCount     uint                           `json:"seconds_count" binding:"required"`
+	StepCount        *uint                          `json:"step_count" binding:"required"`
+	SecondsCount     *uint                          `json:"seconds_count" binding:"required"`
 }
 
 type UpdateTrainingSessionResponse CreateTrainingSessionResponse

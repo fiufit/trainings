0fe3c3dee50c97adf7d1f720370a54c412a2b5a6
TGRodriguez <tgrodriguez@fi.uba.ar>
2023-06-25 14:55:48 -0300

    Added handle error type function

--- contracts/errors.go
+++ contracts/errors.go
@@ -1,6 +1,11 @@
 package contracts
 
-import "errors"
+import (
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 var (
 	ErrInternal                       = errors.New("something went wrong")
@@ -29,3 +34,61 @@ var (
 	ErrTrainingAlreadyDisabled        = errors.New("training plan already disabled")
 	ErrTrainingNotDisabled            = errors.New("training plan not disabled")
 )
+
+func HandleErrorType(ctx *gin.Context, err error) {
+	var status int
+
+	switch {
+	case errors.Is(err, ErrBadRequest):
+		status = http.StatusBadRequest
+	case errors.Is(err, ErrTrainingPlanNotFound):
+		status = http.StatusNotFound
+	case errors.Is(err, ErrUnauthorizedTrainer):
+		status = http.StatusUnauthorized
+	case errors.Is(err, ErrSelfReview):
+		status = http.StatusForbidden
+	case errors.Is(err, ErrReviewAlreadyExists):
+		status = http.StatusConflict
+	case errors.Is(err, ErrReviewNotFound):
+		status = http.StatusNotFound
+	case errors.Is(err, ErrUnauthorizedReviewer):
+		status = http.StatusUnauthorized
+	case errors.Is(err, ErrInvalidTag):
+		status = http.StatusBadRequest
+	case errors.Is(err, ErrTrainingSessionNotFound):
+		status = http.StatusNotFound
+	case errors.Is(err, ErrTrainingSessionNotComplete):
+		status = http.StatusBadRequest
+	case errors.Is(err, ErrTrainingSessionAlreadyFinished):
+		status = http.StatusBadRequest
+	case errors.Is(err, ErrUnauthorizedAthlete):
+		status = http.StatusUnauthorized
+	case errors.Is(err, ErrInvalidGoalType):
+		status = http.StatusBadRequest
+	case errors.Is(err, ErrInvalidGoalSubtype):
+		status = http.StatusBadRequest
+	case errors.Is(err, ErrGoalNotFound):
+		status = http.StatusNotFound
+	case errors.Is(err, ErrAlreadyLiked):
+		status = http.StatusConflict
+	case errors.Is(err, ErrNotLiked):
+		status = http.StatusConflict
+	case errors.Is(err, ErrTrainingAlreadyDisabled):
+		status = http.StatusConflict
+	case errors.Is(err, ErrTrainingNotDisabled):
+		status = http.StatusConflict
+	case errors.Is(err, ErrForeignKey):
+		status = http.StatusConflict
+	case errors.Is(err, ErrUserNotFound):
+		status = http.StatusNotFound
+	case errors.Is(err, ErrExerciseNotFound):
+		status = http.StatusNotFound
+	case errors.Is(err, ErrUserInternal):
+		status = http.StatusInternalServerError
+	case errors.Is(err, ErrUserBadRequest):
+		status = http.StatusBadRequest
+	default:
+		status = http.StatusInternalServerError
+	}
+	ctx.JSON(status, FormatErrResponse(err))
+}
